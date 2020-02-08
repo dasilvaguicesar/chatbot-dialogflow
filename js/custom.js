10bbc5da2cf83b78ca03f2d2ff7b64d123cb6e38
Guilherme Cesar Da Silva <dasilvaguilhermecesar@gmail.com>
2020-02-08 13:08:56 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,11 @@
-$("img").click(function () {$(".chat").toggle("slow");});function sideIn(id) {document.getElementById(id).src = "img/icon-chat-d.png"}function sideOut(id) {document.getElementById(id).src = "img/icon-chat.png"}
+$("img").click(function () {
+  $(".chat").toggle("slow");
+});
+
+function sideIn(id) {
+  document.getElementById(id).src = "img/icon-chat-d.png"
+}
+
+function sideOut(id) {
+  document.getElementById(id).src = "img/icon-chat.png"
+}
